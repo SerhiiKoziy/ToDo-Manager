7216729c63d2484a08f2fa554ee61868463713ce
serhiikoziy <serhiikoziy@gmail.com>
2020-07-09 17:35:49 +0300

    change structure Edit Task

--- src/js/pages/EditTask/index.js
+++ src/js/pages/EditTask/index.js
@@ -1,55 +1,39 @@
-import React, { PureComponent } from 'react';
+import React  from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
+
 import CreateTask from '../../components/Task/CreateTask';
 import Task from "../../components/Task/Task";
+
 import { deleteTask } from "../../actions";
 
-class EditTask extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-
-    this.deleteTask = this.deleteTask.bind(this);
-  }
-
-  static propTypes = {
-    push: React.PropTypes.func,
-    deleteTask: React.PropTypes.func,
-    currentTask: React.PropTypes.object,
-  };
-
-  deleteTask(currentTask) {
+const EditTask = ({ currentTask }) => {
+  const deleteTask = (currentTask) => {
     this.props.push('/');
     this.props.deleteTask(currentTask.eventId);
-  }
+  };
 
-  render() {
-    const { currentTask } = this.props;
-
-    return (
-      <div className="edit-page ">
-        {
-          currentTask && (
-            <Task
-              currentTask={currentTask}
-              onDelete={() => this.deleteTask(currentTask)}
-            />
-          )
-        }
-        <div className="builder-task">
-          <CreateTask
-            key={currentTask.updatedAt}
-            currentTask={currentTask || {}}
-            isEdit={true}
-            buttonText="Edit event"
+  return (
+    <div className="edit-page ">
+      {
+        currentTask && (
+          <Task
+            currentTask={currentTask}
+            onDelete={deleteTask(currentTask)}
           />
-        </div>
+        )
+      }
+      <div className="builder-task">
+        <CreateTask
+          key={currentTask.updatedAt}
+          currentTask={currentTask || {}}
+          isEdit={true}
+          buttonText="Edit event"
+        />
       </div>
-    );
-  }
-}
+    </div>
+  )
+};
 
 export default connect(
   (state, ownProps) => {
