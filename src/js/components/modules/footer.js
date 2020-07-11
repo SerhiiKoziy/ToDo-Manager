5582d799df2c85ed49188cd9301c95d36e665f71
serhiikoziy <serhiikoziy@gmail.com>
2020-07-11 11:23:52 +0300

    change footer

--- src/js/components/modules/footer.js
+++ src/js/components/modules/footer.js
@@ -1,47 +1,29 @@
-import React, { Component } from 'react';
-import connect from 'react-redux/es/connect/connect';
+import React from 'react';
 
 import { Link } from 'react-router';
 
-import { addUserStore } from '../../actions/userStoreActions';
-
 import './footer.scss';
 
-class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      openAuth: false,
-      PhotoUrl: '',
-    };
-  }
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-tab">
+        <Link to="/">
+          <span>Main</span>
+        </Link>
+      </div>
+      <div className="footer-tab">
+        <Link to="/events">
+          <span>Tasks</span>
+        </Link>
+      </div>
+      <div className="footer-tab">
+        <Link to="/profile">
+          <span>Options</span>
+        </Link>
+      </div>
+    </footer>
+  )
+};
 
-  render() {
-    return (
-      <footer className="footer">
-        <div className="footer-tab">
-          <Link to="/">
-            <span>Main</span>
-          </Link>
-        </div>
-        <div className="footer-tab">
-          <Link to="/events">
-            <span>Tasks</span>
-          </Link>
-        </div>
-        <div className="footer-tab">
-          <Link to="/profile">
-            <span>Options</span>
-          </Link>
-        </div>
-      </footer>
-    );
-  }
-}
-
-export default connect(
-  (state) => {
-    return { data: state.data, user: state.user };
-  },
-  { addUserStore }
-)(Footer);
+export default Footer;
