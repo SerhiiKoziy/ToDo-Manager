6b3e66efcc93d6c2a20ddaf818f9f22eed65b06d
serhiikoziy <serhiikoziy@gmail.com>
2020-10-17 19:53:07 +0300

    add datepicker fixes

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,52 +1,132 @@
-import React, { ReactElement } from 'react';
+import React, {ReactElement, useEffect, useState, useMemo, ReactNode, useCallback} from 'react';
 import DateFnsUtils from '@date-io/date-fns';
 import classNames from "classnames";
 import { WrappedFieldProps } from "redux-form";
+import { FormControl, FormHelperText, InputLabel } from '@material-ui/core';
 
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
 
+
+import buildClassName from '../../utils/buildClassName';
 import styles from "./styles.module.scss";
 
 interface IDatePicker {
-  label?: string;
+  label?: ReactNode;
+  inputRef: (ref: HTMLInputElement | null) => void;
+  defaultValue?: string;
+  required?: boolean;
   className?: string;
 }
 
-const DatePicker = ({ className, input,  meta: { touched, invalid, error }, label }: IDatePicker & WrappedFieldProps ): ReactElement => {
+const DATE_FORMAT = 'dd.MM.yyyy';
 
-  // disablePrevDates() {
-  //   const startSeconds = Date.parse(new Date());
-  //   return (date) => {
-  //     const maxDate = startSeconds + (14 * 1000 * 3600 * 24);
-  //     return (Date.parse(date) < startSeconds || Date.parse(date) > maxDate);
-  //   };
-  // }
+const DatePicker = ({ className, input, defaultValue, required, meta: { touched, invalid, error }, inputRef, label, ...rest }: IDatePicker & WrappedFieldProps ): ReactElement => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  const currentValue: Date | null = useMemo(
+    () => input.value ? new Date(input.value) : null,
+    [input.value],
+  );
+
+  useEffect(
+    () => {
+      if (typeof defaultValue !== 'undefined') {
+        handleAsyncChange(defaultValue);
+      }
+    },
+    [defaultValue],
+  );
+
+  const handleAsyncChange = (date: string) => {
+    setIsFocused(false);
+    setHasError(false);
+
+    input.onChange(date);
+  };
+
+  const handleChange = (date: Date | null) => {
+    const stringDate = JSON.parse(JSON.stringify(date));
+
+    handleAsyncChange(stringDate);
+    input.onBlur(stringDate);
+  };
+
+  const handleError = (error: ReactNode) => setHasError(!!error);
+
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
+  const setInputRef = (ref: HTMLInputElement | null) => {
+    if (typeof inputRef === 'function') {
+      inputRef(ref && ref.children[0].children[0] as HTMLInputElement);
+    }
+  };
+
+  const disabledDates = useCallback(
+    (date) => {
+      const currentDate = Date.parse(`${new Date()}`);
+      const maxDateFromCurrent = currentDate + (14 * 1000 * 3600 * 24); // 14 days //TODO make logic without seconds
+
+      return Date.parse(date) > maxDateFromCurrent;
+    },
+    [],
+  );
 
   return (
-    <div className={classNames(styles.datepickerWr, className)}>
-      <div className={styles.datepickerField}>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <KeyboardDatePicker
-            id="date-picker-inline"
-            disableToolbar
-            variant="inline"
-            format="MM/dd/yyyy"
-            margin="normal"
-            label={label}
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <FormControl
+        className={
+          buildClassName(
+            styles.advancedDatePickerField,
+            // {
+            //   [styles.withoutError]: (!touched || !invalid) && !hasError,
+            // },
+          )
+        }
+        error={touched && !!error}
+      >
+
+        {label && (
+          <InputLabel
+            className={styles.label}
+            focused={isFocused}
             error={touched && invalid}
-            className={styles.datepicker}
-            helperText={touched && error}
-            {...input}
-            KeyboardButtonProps={{
-              'aria-label': 'change date',
-            }}
-          />
-        </MuiPickersUtilsProvider>
-      </div>
-    </div>
+            required={required}
+            shrink
+          >
+            {label}
+          </InputLabel>
+        )}
+
+        <KeyboardDatePicker
+          variant="inline"
+          format={DATE_FORMAT}
+          value={currentValue || null}
+          innerRef={setInputRef}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          onFocus={handleFocus}
+          autoOk
+          onError={handleError}
+          FormHelperTextProps={{
+            className: styles.errorHelper,
+          }}
+          shouldDisableDate={disabledDates}
+          maxDate={'2027-01-01'}
+          disablePast
+          {...rest}
+        />
+
+        {touched && error && (
+          <FormHelperText className={styles.errorHelper}>{error}</FormHelperText>
+        )}
+      </FormControl>
+    </MuiPickersUtilsProvider>
   )
 };
 
