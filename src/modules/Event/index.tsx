db18338f719e8726b78714d249b96f1e6ad99f01
serhiikoziy <serhiikoziy@gmail.com>
2020-10-21 19:36:37 +0300

    event fixes

--- src/modules/Event/index.tsx
+++ src/modules/Event/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import classNames from "classnames";
+import moment from 'moment';
 // import SimpleMap from "./GoogleMap";
 
-import Link from "../../components/Link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTemperatureLow, faEye, faPencilRuler, faTrash } from '@fortawesome/free-solid-svg-icons';
+import Link from "../../components/Link";
 
 import IEvent from '../../types/IEvent';
 import IWeather from "../../types/IWeather";
+import IWeatherTemp from "../../types/IWeatherTemp";
 
 import styles from './styles.module.scss';
 
@@ -19,76 +21,51 @@ interface IEventProps {
 }
 
 export const Event = ({ event, onDelete, onEditEvent, className }: IEventProps): any => {
-  const weather: IWeather = event && event.weather;
-  // const cloudImageUrl = weather && `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
-
-  const renderWeather = () => {
-    const weather: IWeather = event.weather;
-
-    return weather && Object.keys(weather).map((key: any) => {
-      if (key !== 'weather' && key !== 'temp') {
-        return (
-          <div key={key}>
-            <span>{key} :</span>
-            {/*<span>{weather[key]}</span>*/}
-          </div>
-        );
-      }
-
-      return null;
-    });
-  };
-
-  // const renderMap = (currentTask: any) => {
-  //   return (
-  //     <SimpleMap
-  //       key={JSON.stringify(currentTask.position)}
-  //       position={currentTask.position}
-  //     />
-  //   );
-  // };
+  const { temp, weather }: IWeather = event && event.weather;
+  const { description, main, icon }: IWeatherTemp = weather[0];
+  const cloudImageUrl = `http://openweathermap.org/img/w/${icon}.png`;
 
   return (
-    <div
-      // className={`task ${currentTask.stageProces} ${parentWr}`}
-      className={
-        classNames(
-          className,
-          styles.task,
-          // {
-          //   [styles.link]: styled,
-          // },
-        )
-      }
-      // id={currentTask.id}
-    >
+    <div className={classNames(className, styles.task)}>
       <div className={styles.taskContent}>
         <div className={styles.taskMainInfo}>
+
           <h4>Title: {event.title}</h4>
           <p>Desc: {event.description}</p>
-          <p>{`${event.date}`}</p>
+          <p>{`${moment(event.date).format('LL')}`}</p>
           <p>{`${event.address}`}</p>
+
           <p>
-            <FontAwesomeIcon icon={faTemperatureLow} /> Temperature celsius:
+            <FontAwesomeIcon icon={faTemperatureLow} /> <br/>
+            Temperature celsius:
           </p>
+
           {
-            weather && (
+            temp && (
               <ul className={styles.tempList}>
-                <li>Day: {`${weather.temp.day}`}</li>
-                <li>Evening: {`${weather.temp.eve}`}</li>
-                <li>Morning: {`${weather.temp.morn}`}</li>
-                <li>Night: {`${weather.temp.night}`}</li>
+                <li className={styles.temp}>Day: {`${temp.day}`}</li>
+                <li className={styles.temp}>Evening: {`${temp.eve}`}</li>
+                <li className={styles.temp}>Morning: {`${temp.morn}`}</li>
+                <li className={styles.temp}>Night: {`${temp.night}`}</li>
               </ul>
             )
           }
         </div>
         <div className={styles.taskSecondaryInfo}>
-          {renderWeather()}
+          <p>Second info:</p>
+          {
+            weather[0] && (
+              <ul className={styles.tempList}>
+                <li>Description: { description }</li>
+                <li>Main: { main }</li>
+              </ul>
+            )
+          }
         </div>
 
-        {/*<div className={styles.weatherIndicator}>*/}
-        {/*  <img src={cloudImageUrl} alt="" />*/}
-        {/*</div>*/}
+        <div className={styles.weatherIndicator}>
+          <img src={cloudImageUrl} alt="" />
+        </div>
       </div>
 
       {/*{*/}
@@ -106,10 +83,6 @@ export const Event = ({ event, onDelete, onEditEvent, className }: IEventProps):
           </Link>
         </div>
         <div className={styles.control}>
-          {/*<Link href={`/task/${event.eventId}/edit`}>*/}
-          {/*  <FontAwesomeIcon icon={faPencilRuler} />*/}
-          {/*</Link>*/}
-
           <div onClick={() => onEditEvent(event.eventId)}>
             <FontAwesomeIcon icon={faPencilRuler} />
           </div>
